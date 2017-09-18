1defadb7b48ad829e51cc1e1ea0ed3d48ca8e1f2
andersoncardoso <anderson.d.cardoso@hotmail.com>
2017-09-18 09:59:34 -0500

    Initial commit

---
+++ src/HelloWorld.java
@@ -0,0 +1,9 @@
+public class HelloWorld {
+    // psvm
+    //entry point
+    public static void main(String[] args) {
+        //sout
+        System.out.println("hello world!");
+        System.out.print("hello Redwood!");
+    }
+}
