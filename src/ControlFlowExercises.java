459cb7f422733c8b6e58225a78f190abc0743fa0
andersoncardoso <anderson.d.cardoso@hotmail.com>
2017-09-19 14:18:26 -0500

    while loop exercise

---
+++ src/ControlFlowExercises.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class ControlFlowExercises {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        int i = 5;
+
+        while(i <= 15) {
+            System.out.println(i);
+            i++;
+
+        }
+    }
+}
---
+++ src/JavaSyntax.java
@@ -0,0 +1,14 @@
+public class JavaSyntax {
+
+    public static void main(String[] args) {
+        // VARIABLES.
+        int myFavoriteNumber = 25;
+        String myString = "Anderson Cardoso";
+        float myNumber = 3.14f;
+
+        // CONSOLE-LOGS.
+        System.out.println(myFavoriteNumber);
+        System.out.println(myString);
+        System.out.println(myNumber);
+    }
+}
