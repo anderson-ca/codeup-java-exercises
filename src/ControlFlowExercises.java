d20bcf543599ac168c0e4429208eb24dca57db95
andersoncardoso <anderson.d.cardoso@hotmail.com>
2017-09-20 08:50:04 -0500

    table exercise

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -6,14 +6,30 @@ public class ControlFlowExercises {
         /////////////////////////////////////////////
         /////////////////////////////////////////////
         /////////////////////////////////////////////
-        for (int i = 0; i < 100; i++) {
-            if(i % 3 == 0 && i % 5 == 0) {
-                System.out.println("FizzBuzz");
-            } else if(i % 5 == 0) {
-                System.out.println("Buzz");
-            } else if(i % 3 == 0) {
-                System.out.println("Fizz");
-            }
+        System.out.println("Enter integer: ");
+
+        int num = input.nextInt();
+
+        int cubed;
+
+        int squared;
+
+        int test = 0;
+        System.out.println("number  |   square  | cube");
+        System.out.println("---------------------------");
+        for (int i = 0; i <= num; i++) {
+
+            squared = i * i;
+
+            cubed = i * i * i;
+
+            System.out.println();
+
+            System.out.printf("%-5s", i);
+
+            System.out.printf("%5s", squared);
+
+            System.out.printf("%10s", cubed);
 
         }
 
